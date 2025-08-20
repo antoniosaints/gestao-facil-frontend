aed38b1dda79e873c5531cb93ff79bab1a39c1ce
antoniosaints <costaantonio883@gmail.com>
2025-08-19 22:40:52 -0300

    feat(shadcn) adicionado shadcn

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,54 +1,84 @@
 /** @type {import('tailwindcss').Config} */
 export default {
   content: ['./index.html', './src/**/*.{js,ts,jsx,tsx,vue,html}'],
-  darkMode: 'class',
+  darkMode: ['class'],
   theme: {
-    extend: {
-      colors: {
-        background: {
-          DEFAULT: '#fff', // cinza claro
-          dark: '#111827', // cinza escuro
-          hover: '#1f2937', // cinza escuro com hover
-          sidebar: '#111827', // cinza escuro
-        },
-        card: {
-          DEFAULT: '#f9fafb', // cinza claro
-          dark: '#1f2937', // cinza escuro
-        },
-        border: {
-          DEFAULT: '#e5e7eb', // cinza claro
-          dark: '#374151', // cinza escuro
-        },
-        text: {
-          DEFAULT: '#111827', // cinza escuro
-          dark: '#f3f4f6', // cinza claro
-        },
-        primary: {
-          DEFAULT: '#f97316', // laranja
-          dark: '#ea580c',
-        },
-        secondary: {
-          DEFAULT: '#0a48c4', // azul
-          dark: '#042b7a',
-        },
-        success: {
-          DEFAULT: '#16a34a',
-          dark: '#15803d',
-        },
-        warning: {
-          DEFAULT: '#f59e0b',
-          dark: '#d97706',
-        },
-        danger: {
-          DEFAULT: '#ef4444',
-          dark: '#dc2626',
-        },
-        info: {
-          DEFAULT: '#3b82f6',
-          dark: '#2563eb',
-        },
-      },
-    },
+  	extend: {
+  		colors: {
+  			background: {
+  				DEFAULT: 'hsl(var(--background))',
+  				dark: 'hsl(var(--background))',
+  				hover: 'hsl(var(--background))'
+			},
+  			card: {
+  				DEFAULT: 'hsl(var(--card))',
+  				dark: 'hsl(var(--card))',
+  				foreground: 'hsl(var(--card-foreground))'
+  			},
+  			border: 'hsl(var(--border))',
+  			text: {
+  				DEFAULT: '#111827',
+  				dark: '#f3f4f6'
+  			},
+  			primary: {
+  				DEFAULT: 'hsl(var(--primary))',
+  				dark: 'hsl(var(--primary))',
+  				foreground: 'hsl(var(--primary-foreground))'
+  			},
+  			secondary: {
+  				DEFAULT: 'hsl(var(--secondary))',
+  				dark: 'hsl(var(--secondary))',
+  				foreground: 'hsl(var(--secondary-foreground))'
+  			},
+  			success: {
+  				DEFAULT: '#16a34a',
+  				dark: '#15803d'
+  			},
+  			warning: {
+  				DEFAULT: '#f59e0b',
+  				dark: '#d97706'
+  			},
+  			danger: {
+  				DEFAULT: '#ef4444',
+  				dark: '#dc2626'
+  			},
+  			info: {
+  				DEFAULT: '#3b82f6',
+  				dark: '#2563eb'
+  			},
+  			foreground: 'hsl(var(--foreground))',
+  			popover: {
+  				DEFAULT: 'hsl(var(--popover))',
+  				foreground: 'hsl(var(--popover-foreground))'
+  			},
+  			muted: {
+  				DEFAULT: 'hsl(var(--muted))',
+  				foreground: 'hsl(var(--muted-foreground))'
+  			},
+  			accent: {
+  				DEFAULT: 'hsl(var(--accent))',
+  				foreground: 'hsl(var(--accent-foreground))'
+  			},
+  			destructive: {
+  				DEFAULT: 'hsl(var(--destructive))',
+  				foreground: 'hsl(var(--destructive-foreground))'
+  			},
+  			input: 'hsl(var(--input))',
+  			ring: 'hsl(var(--ring))',
+  			chart: {
+  				'1': 'hsl(var(--chart-1))',
+  				'2': 'hsl(var(--chart-2))',
+  				'3': 'hsl(var(--chart-3))',
+  				'4': 'hsl(var(--chart-4))',
+  				'5': 'hsl(var(--chart-5))'
+  			}
+  		},
+  		borderRadius: {
+  			lg: 'var(--radius)',
+  			md: 'calc(var(--radius) - 2px)',
+  			sm: 'calc(var(--radius) - 4px)'
+  		}
+  	}
   },
-  plugins: [],
+  plugins: [require("tailwindcss-animate")],
 }
