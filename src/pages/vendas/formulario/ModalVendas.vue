91baba5d7a230bda9bc690a45ce2f2603bf7e2f1
antoniosaints <costaantonio883@gmail.com>
2025-08-26 21:57:41 -0300

    feat(ui) ajustes de UI

--- src/pages/vendas/formulario/ModalVendas.vue
+++ src/pages/vendas/formulario/ModalVendas.vue
@@ -5,16 +5,280 @@ import { Button } from "@/components/ui/button";
 import { Input } from "@/components/ui/input";
 import { NumberField, NumberFieldContent, NumberFieldDecrement, NumberFieldIncrement, NumberFieldInput } from "@/components/ui/number-field";
 import { Select, SelectContent, SelectItem, SelectTrigger, SelectValue } from "@/components/ui/select";
+import http from "@/utils/axios";
 import { colorTheme } from "@/utils/theme";
-import { ptBR } from "date-fns/locale";
+import { id, ptBR } from "date-fns/locale";
 import { ref } from "vue";
+import { useToast } from "vue-toastification";
 
 const title = ref('Cadastro de venda')
 const description = ref('Preencha os campos abaixo')
+const toast = useToast()
+
+interface CarrinhoItem {
+    id: number;
+    produto: string;
+    quantidade: number;
+    preco: number;
+    subtotal: number;
+}
+
+const carrinho = ref<CarrinhoItem[]>(localStorage.getItem('gestao_facil:cartVendas') ? JSON.parse(localStorage.getItem('gestao_facil:cartVendas') as string) : [])
+
+const formularioVenda = ref({
+    id: null,
+    vendedorId: null,
+    clienteId: null,
+    dataVenda: null,
+    tipoDesconto: 'PORCENTAGEM',
+    desconto: 0,
+    valorTotal: 0,
+    garantia: '',
+    observacoes: '',
+    itens: []
+})
+
+const addItemForm = ref<{ id: number | null, preco: number | null, quantidade: number }>({
+    id: null,
+    preco: null,
+    quantidade: 1
+})
+
+const propostaPreco = ref<string | null>('')
 
 const open = defineModel({ default: false })
 
 const dataVenda = ref(new Date())
+
+
+function getValorTotalCarrinho() {
+    return carrinho.value.reduce((total, item) => total + item.subtotal, 0);
+}
+
+function proporValorVendaCliente(valorProposto: number) {
+    if (!valorProposto) return;
+
+    const valorCarrinho = parseFloat(String(getValorTotalCarrinho())) || 0; // subtotal
+    const valorPropostoNum = parseFloat(String(valorProposto)) || 0;
+
+    // Se o valor proposto for maior ou igual ao subtotal, desconto = 0
+    if (valorPropostoNum >= valorCarrinho) {
+        formularioVenda.value.desconto = 0;
+        atualizarTabelaCart();
+        return;
+    }
+    formularioVenda.value.tipoDesconto = 'VALOR';
+
+    // Desconto necessário para chegar no valor desejado
+    const valorDescontoNovo = valorCarrinho - valorPropostoNum;
+    formularioVenda.value.desconto = valorDescontoNovo;
+    atualizarTabelaCart();
+}
+
+function definirValorProporcionalVenda() {
+    const valorTratado = propostaPreco.value ? parseFloat(propostaPreco.value.replace(',', '.')) : 0;
+    proporValorVendaCliente(valorTratado);
+    propostaPreco.value = null;
+}
+
+function setCartFromVendaRealizada(venda: { ItensVendas: any[] }) {
+    venda.ItensVendas.forEach(item => {
+        const newItem = {
+            id: item.id,
+            produto: item.produto.nome,
+            quantidade: item.quantidade,
+            preco: parseFloat(item.valor.replace(',', '.')),
+            subtotal: parseFloat(item.valor.replace(',', '.')) * item.quantidade
+        };
+        carrinho.value.push(newItem);
+    })
+
+    localStorage.setItem('gestao_facil:cartVendas', JSON.stringify(carrinho.value));
+    atualizarTabelaCart();
+}
+
+async function loadVendaEdicao() {
+    try {
+        if (formularioVenda.value.id) {
+            const data = await http.get("/vendas/" + formularioVenda.value.id)
+            formularioVenda.value = data.data;
+        }
+    } catch (error) {
+        console.log(error);
+    }
+}
+
+loadVendaEdicao();
+
+// $("#formulario_cadastro_venda").submit((e) => {
+//     e.preventDefault();
+
+//     const cart = localStorage.getItem('gestao_facil:cartVendas');
+//     const cartItems = cart ? JSON.parse(cart) : [];
+
+//     if (cartItems.length === 0) {
+//         Swal.fire({
+//             icon: "error",
+//             title: "Erro!",
+//             text: "Nenhum item adicionado na venda",
+//             confirmButtonText: "Ok",
+//         });
+//         return;
+//     }
+
+//     const data = {
+//         clienteId: $('#select_cliente_venda_formulario').val(),
+//         data: $('#input_data_venda_formulario').val(),
+//         vendedorId: $('#select_vendedor_venda_formulario').val(),
+//         status: $('#select_status_venda_formulario').val(),
+//         garantia: $('#input_garantia_venda_formulario').val(),
+//         observacoes: $('#input_observacoes_venda_formulario').val(),
+//         desconto: getValorDesconto(),
+//         id: $("#input_id_venda_formulario").val(),
+//         itens: cartItems.map(item => (
+//             {
+//                 id: item.id,
+//                 quantidade: item.quantidade,
+//                 preco: item.preco
+//             }
+//         ))
+//     };
+//     const hasId = $("#input_id_venda_formulario").val();
+//     $.ajax({
+//         url: `/api/vendas/criar${hasId ? `?id=${hasId}` : ''}`,
+//         method: 'POST',
+//         data: data,
+//         dataType: 'json',
+//         headers: {
+//             'Authorization': `Bearer ${localStorage.getItem('gestao_facil:token')}`
+//         },
+//         success: function (data) {
+//             showNotification('Venda salva com sucesso!', 'success');
+//             loadPage('/vendas/resumo');
+//         },
+//         error: function (xhr, status, error) {
+//             console.log(xhr);
+//             const mensagem =
+//                 xhr.responseJSON?.message || "Erro inesperado na requisição";
+//             showNotification(mensagem, 'error');
+//         }
+//     });
+// });
+
+
+function clearFormularioAddItem() {
+    addItemForm.value.id = null;
+    addItemForm.value.preco = null;
+    addItemForm.value.quantidade = 1;
+}
+
+function addToCartVendas() {
+    if (!addItemForm.value.id || !addItemForm.value.quantidade || !addItemForm.value.preco) {
+        return toast.error('Preencha todos os campos (Produto, Quantidade e Preço)');
+    }
+
+    // Verifica se o produto já foi adicionado
+    const exists = carrinho.value.some(item => item.id === addItemForm.value.id);
+    if (exists) {
+        return toast.error('Este produto já está na lista.');
+    }
+
+    const newItem = {
+        id: addItemForm.value.id,
+        produto: "Produto " + addItemForm.value.id,
+        quantidade: addItemForm.value.quantidade,
+        preco: addItemForm.value.preco,
+        subtotal: +(addItemForm.value.preco * addItemForm.value.quantidade)
+    };
+
+    carrinho.value.push(newItem);
+    localStorage.setItem('gestao_facil:cartVendas', JSON.stringify(carrinho.value));
+
+    atualizarTabelaCart();
+    clearFormularioAddItem();
+}
+
+
+function removeFromCartVendas(produtoId: number) {
+    const novoCarrinho = carrinho.value.filter(item => Number(item.id) !== Number(produtoId));
+
+    carrinho.value = novoCarrinho;
+    localStorage.setItem('gestao_facil:cartVendas', JSON.stringify(novoCarrinho));
+    atualizarTabelaCart();
+}
+
+function calculateTotalCartVendas() {
+    const cart = localStorage.getItem('gestao_facil:cartVendas');
+    const cartItems: CarrinhoItem[] = cart ? JSON.parse(cart) : [];
+
+    const total = cartItems.reduce((sum, item) => {
+        return sum + item.subtotal;
+    }, 0);
+
+    return total.toFixed(2);
+}
+
+function clearCartVendas() {
+    localStorage.removeItem('gestao_facil:cartVendas');
+    atualizarTabelaCart();
+}
+
+function getValorDesconto() {
+    let descontoTotal = 0;
+
+    if (carrinho.value.length === 0) {
+        return descontoTotal;
+    }
+
+    const subtotalValue = carrinho.value.reduce((total, item) => total + (item.preco * item.quantidade), 0);
+
+    if (formularioVenda.value.desconto && !isNaN(formularioVenda.value.desconto)) {
+        if (formularioVenda.value.tipoDesconto === 'PORCENTAGEM') {
+            descontoTotal = subtotalValue * (formularioVenda.value.desconto / 100);
+        } else {
+            descontoTotal = formularioVenda.value.desconto;
+        }
+    } else {
+        descontoTotal = 0;
+    }
+
+    return descontoTotal;
+}
+
+function atualizarTabelaCart() {
+    // const cartList = $('#lista-carrinho-vendas');
+    // const resumoCarrinho = $('#resumo-carrinho-vendas');
+    // const subtotalCarrinho = $('#subtotal-carrinho-vendas');
+    // const totalCarrinho = $('#total-carrinho-vendas');
+    // const descontoCarrinho = $('#desconto-total-carrinho');
+
+    // cartList.empty();
+
+    // if (cartItems.length === 0) {
+    //     cartList.append(`
+    //         <div class="p-3 text-center text-gray-500 bg-gray-50 dark:bg-gray-800 rounded-md">
+    //             Nenhum item adicionado
+    //         </div>
+    //     `);
+    //     descontoCarrinho.text(`R$ 0,00`);
+    //     subtotalCarrinho.text(`R$ 0,00`);
+    //     totalCarrinho.text(`R$ 0,00`);
+    //     return;
+    // }
+
+    // let total = 0;
+
+    // cartItems.forEach(item => {
+    //     total += item.subtotal;
+    // });
+
+    // // Exibe total no resumo
+    // descontoCarrinho.text(`R$ ${getValorDesconto().toFixed(2).replace('.', ',')}`);
+    // subtotalCarrinho.text(`R$ ${total.toFixed(2).replace('.', ',')}`);
+    // totalCarrinho.text(`R$ ${(total - getValorDesconto()).toFixed(2).replace('.', ',')}`);
+}
+
+clearCartVendas();
 </script>
 
 <template>
@@ -119,13 +383,13 @@ const dataVenda = ref(new Date())
             <div class="grid grid-cols-1 md:grid-cols-12 gap-4 items-end">
                 <div class="md:col-span-6">
                     <label class="block text-sm mb-1">Produto <span class="text-red-500">*</span></label>
-                    <Select2Ajax class="w-full" url="/produtos/select2" :allow-clear="true" />
+                    <Select2Ajax v-model="addItemForm.id" class="w-full" url="/produtos/select2" :allow-clear="true" />
                 </div>
 
                 <div class="md:col-span-2">
                     <label for="quantidade_carrinho_adicionar" class="block text-sm mb-1">Quantidade <span
                             class="text-red-500">*</span></label>
-                    <NumberField class="bg-card dark:bg-card-dark" id="quantidade_carrinho_adicionar" :default-value="1"
+                    <NumberField v-model="addItemForm.quantidade" class="bg-card dark:bg-card-dark" id="quantidade_carrinho_adicionar" :default-value="1"
                         :min="1">
                         <NumberFieldContent>
                             <NumberFieldDecrement />
@@ -137,12 +401,12 @@ const dataVenda = ref(new Date())
 
                 <div class="md:col-span-2">
                     <label class="block text-sm mb-1">Preço <span class="text-red-500">*</span></label>
-                    <Input type="text" placeholder="R$ 0,00" id="input_preco_venda_formulario"
+                    <Input v-model="(addItemForm.preco as number)" type="text" placeholder="R$ 0,00" id="input_preco_venda_formulario"
                         class="w-full p-2 rounded-md border bg-card dark:bg-card-dark border-border dark:border-border-dark" />
                 </div>
 
                 <div class="md:col-span-2">
-                    <Button type="button" class="text-white w-full">
+                    <Button type="button" @click="addToCartVendas" class="text-white w-full">
                         <i class="fa-solid fa-cart-plus"></i>
                         Adicionar
                     </Button>
@@ -166,8 +430,26 @@ const dataVenda = ref(new Date())
                     <div class="grid grid-cols-12 gap-4">
                         <!-- Lista do carrinho -->
                         <div id="lista-carrinho-vendas" class="col-span-12 lg:col-span-8 space-y-2">
-                            <div class="p-3 text-center text-gray-500 bg-gray-50 dark:bg-gray-800 rounded-md">
+                            <div v-if="carrinho.length === 0" class="p-3 text-center text-gray-500 bg-gray-50 dark:bg-gray-800 rounded-md">
                                 Nenhum item adicionado
+                            </div>
+                            <div
+                                v-else
+                                v-for="item in carrinho"
+                                :key="item.id"
+                                class="flex justify-between items-center bg-white dark:bg-gray-900 border border-gray-200 dark:border-gray-700 rounded-md p-2 shadow-sm">
+                                <div class="flex flex-col text-sm">
+                                    <span class="font-medium text-gray-800 dark:text-gray-200">{{ item.produto }}</span>
+                                    <span class="text-gray-500 dark:text-gray-400">Qtd: {{ item.quantidade }}</span>
+                                </div>
+                                <div class="flex flex-col text-right text-sm">
+                                    <span class="font-medium text-gray-800 dark:text-gray-200">R$ {{ item.preco }}</span>
+                                    <span class="text-gray-500 dark:text-gray-400">Subtotal: R$ {{ item.subtotal }}</span>
+                                </div>
+                                <button type="button" @click="removeFromCartVendas(item.id)"
+                                    class="ml-3 text-red-900 bg-red-200 dark:text-red-100 dark:bg-red-800 py-1 px-2 rounded-sm">
+                                    Remover
+                                </button>
                             </div>
                         </div>
 
