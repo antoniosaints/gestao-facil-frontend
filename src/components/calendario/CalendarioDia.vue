3a20a25b76490e7b0fdedbbe2a21565a11382c3d
antoniosaints <costaantonio883@gmail.com>
2025-09-08 23:05:04 -0300

    feat(ui) ajustes de evetos do dia

--- src/components/calendario/CalendarioDia.vue
+++ src/components/calendario/CalendarioDia.vue
@@ -1,5 +1,7 @@
 <script setup lang="ts">
-import { format } from "date-fns"
+import { addHours, format, isSameHour, startOfDay } from "date-fns"
+import { ArrowBigRight, Plus } from "lucide-vue-next";
+import Button from "../ui/button/Button.vue";
 
 const props = defineProps<{ eventos: { id: number; titulo: string; data: string, fim: string }[] }>()
 
@@ -7,18 +9,38 @@ const hoje = new Date()
 const dataHoje = format(hoje, "yyyy-MM-dd")
 
 const eventosHoje = props.eventos.filter(e => format(e.data, "yyyy-MM-dd") === dataHoje)
+
+const inicioDia = startOfDay(hoje)
+const horas = Array.from({ length: 11 }, (_, i) => addHours(inicioDia, i + 8))
 </script>
 
 <template>
-    <div class="border rounded p-4">
-        <h3 class="font-bold mb-2">Eventos do dia {{ format(new Date(), "dd/MM/yyyy") }}</h3>
-        <div v-if="eventosHoje.length">
-            <div v-for="ev in eventosHoje" :key="ev.id" class="mb-1 px-2 py-1 bg-primary text-white rounded truncate">
-                {{ format(new Date(ev.data), "HH:mm") }} -
-                {{ format(new Date(ev.fim), "HH:mm") }}
-                {{ ev.titulo }}
+
+    <div>
+        <div v-for="hora in horas" :key="hora.toISOString()"
+            class="flex items-center space-x-4 p-3 border relative rounded-lg mb-2">
+            <span
+                class="absolute left-[-.85rem] bg-gray-50 dark:dark:bg-gray-900 border-2 p-1 border-primary rounded-full">
+                <ArrowBigRight class="h-4 w-4" />
+            </span>
+            <div class="w-16 text-sm font-medium text-muted-foreground">
+                {{ format(hora, "HH:mm") }}
             </div>
+            <div class="flex-1 grid grid-cols-1 md:grid-cols-2 lg:grid-cols-4 gap-2">
+                <div v-for="ev in eventos.filter(e => isSameHour(e.data, hora))"
+                    class="px-3 py-2 rounded-lg bg-blue-100 text-blue-800 dark:bg-blue-800 dark:text-blue-100">
+                    <div class="font-medium truncate">
+                        {{ ev.titulo }}
+                    </div>
+                    <div class="text-xs">
+                        {{ format(new Date(ev.data), "HH:mm") }} -
+                        {{ format(new Date(ev.fim), "HH:mm") }}
+                    </div>
+                </div>
+            </div>
+            <Button variant="outline" size="sm" class="flex-shrink-0" title="Reserva rápida">
+                <Plus class="h-4 w-4" />
+            </Button>
         </div>
-        <div v-else class="text-gray-500 text-sm">Nenhuma ordem hoje.</div>
     </div>
 </template>
