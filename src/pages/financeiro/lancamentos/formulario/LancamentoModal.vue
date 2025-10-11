0e00c08406c23cf65f02d41309626c7fd8b1bdcb
antoniosaints <costaantonio883@gmail.com>
2025-10-11 19:41:24 -0300

    feat(ui) ajuste

--- src/pages/financeiro/lancamentos/formulario/LancamentoModal.vue
+++ src/pages/financeiro/lancamentos/formulario/LancamentoModal.vue
@@ -66,18 +66,24 @@ async function submit() {
 <template>
     <ModalView v-model:open="store.openModal" :title="title" :description="description" size="3xl">
         <form class="space-y-4 px-4" @submit.prevent="submit">
-            <div class="grid grid-cols-1 gap-6">
-                <div>
-                    <label for="descricao" class="block text-sm font-medium mb-1">
-                        Descrição *
-                    </label>
-                    <Input v-model="store.form.descricao" type="text" id="descricao" name="descricao" required
-                        placeholder="Ex: Venda de 1 notebook" />
-                </div>
-
-                <div class="grid grid-cols-1 md:grid-cols-3 gap-2">
+            <div class="grid grid-cols-1 gap-3">
+                <div class="grid grid-cols-1 md:grid-cols-12 gap-3">
+                    <div class="md:col-span-8">
+                        <label for="descricao" class="block text-sm font-medium mb-1">
+                            Descrição *
+                        </label>
+                        <Input v-model="store.form.descricao" type="text" id="descricao" name="descricao" required
+                            placeholder="Ex: Venda de 1 notebook" />
+                    </div>
+                    <div class="md:col-span-4">
+                        <label for="dataFinanceiroLancamento" class="block text-sm font-medium mb-1">
+                            Data Lançamento *
+                        </label>
+                        <Calendarpicker id="dataFinanceiroLancamento" name="dataLancamento"
+                            v-model="store.form.dataLancamento" />
+                    </div>
                     <!-- Tipo -->
-                    <div>
+                    <div class="md:col-span-4">
                         <label for="tipoLancamentoFinanceiro" class="block text-sm font-medium mb-1">
                             Tipo/Natureza *
                         </label>
@@ -97,15 +103,23 @@ async function submit() {
                     </div>
 
                     <!-- Valor Total -->
-                    <div>
+                    <div class="md:col-span-4">
                         <label for="valorTotalLancamento" class="block text-sm font-medium mb-1">
                             Valor Total *
                         </label>
                         <Input type="text" v-maska="moneyMaskOptions" id="valorTotalLancamento"
                             v-model="store.form.valorTotal" name="valorTotal" required placeholder="0,00" />
                     </div>
+
+                    <div class="md:col-span-4">
+                        <label for="descontoFormularioLancamento" class="block text-sm font-medium mb-1">
+                            Desconto
+                        </label>
+                        <Input type="text" v-maska="moneyMaskOptions" v-model="store.form.desconto"
+                            id="descontoFormularioLancamento" name="desconto" placeholder="0,00" />
+                    </div>
                     <!-- Valor Total -->
-                    <div>
+                    <div class="md:col-span-6">
                         <label for="metodoLancamentoModoLancamento" class="block text-sm font-medium mb-1">
                             Método *
                         </label>
@@ -124,47 +138,56 @@ async function submit() {
                         </Select>
                     </div>
 
-                </div>
-
-                <div v-if="params.metodo === 'PARCELADO'" class="grid grid-cols-1 md:grid-cols-3 gap-2">
-                    <!-- Parcelas -->
-                    <div>
-                        <label for="parcelas" class="block text-sm font-medium mb-1">
-                            Parcelas *
-                        </label>
-                        <Input type="number" id="parcelas" name="parcelas" placeholder="1" required min="1"
-                            v-model="store.form.parcelas" />
-                    </div>
-                    <!-- Valor Entrada -->
-                    <div>
-                        <label for="valorEntradaLancamento" class="block text-sm font-medium mb-1">
-                            Valor Entrada
-                        </label>
-                        <Input type="text" v-maska="moneyMaskOptions" v-model="store.form.valorEntrada"
-                            id="valorEntradaLancamento" name="valorEntrada" placeholder="0,00" />
-                    </div>
-
-                    <!-- Data Entrada -->
-                    <div>
-                        <label for="dataEntradaLancamento" class="block text-sm font-medium mb-1">
-                            Data Entrada
-                        </label>
-                        <Calendarpicker :required="store.form.valorEntrada != ''" id="dataEntradaLancamento"
-                            name="dataEntrada" v-model="store.form.dataEntrada" />
-                    </div>
-                </div>
-                <div class="grid grid-cols-1 md:grid-cols-3 gap-2">
-                    <!-- Desconto -->
-                    <div>
-                        <label for="descontoFormularioLancamento" class="block text-sm font-medium mb-1">
-                            Desconto
-                        </label>
-                        <Input type="text" v-maska="moneyMaskOptions" v-model="store.form.desconto"
-                            id="descontoFormularioLancamento" name="desconto" placeholder="0,00" />
+                    <div class="block text-sm font-medium mb-1 md:col-span-6">
+                        <span class="block text-sm font-medium mb-0">
+                            Efetivado
+                        </span>
+                        <div class="gap-2 mt-1 items-center">
+                            <div>
+                                <label for="lancamentoEfetivadoTotal"
+                                    class="border cursor-pointer bg-card dark:bg-card-dark border-border px-3 h-[36px] flex rounded-lg">
+                                    <div class="flex items-center">
+                                        <label class="relative inline-flex items-center cursor-pointer">
+                                            <Switch id="lancamentoEfetivadoTotal"
+                                                v-model="params.lancamentoEfetivado" />
+                                            <span
+                                                class="ml-2 text-sm font-medium text-gray-900 dark:text-gray-300">Efetivado</span>
+                                        </label>
+                                    </div>
+                                </label>
+                            </div>
+                        </div>
                     </div>
 
+                    <div v-if="params.metodo === 'PARCELADO'" class="grid grid-cols-1 md:grid-cols-3 gap-3 md:col-span-12">
+                        <!-- Parcelas -->
+                        <div>
+                            <label for="parcelas" class="block text-sm font-medium mb-1">
+                                Parcelas *
+                            </label>
+                            <Input type="number" id="parcelas" name="parcelas" placeholder="1" required min="1"
+                                v-model="store.form.parcelas" />
+                        </div>
+                        <!-- Valor Entrada -->
+                        <div>
+                            <label for="valorEntradaLancamento" class="block text-sm font-medium mb-1">
+                                Valor Entrada
+                            </label>
+                            <Input type="text" v-maska="moneyMaskOptions" v-model="store.form.valorEntrada"
+                                id="valorEntradaLancamento" name="valorEntrada" placeholder="0,00" />
+                        </div>
+    
+                        <!-- Data Entrada -->
+                        <div>
+                            <label for="dataEntradaLancamento" class="block text-sm font-medium mb-1">
+                                Data Entrada
+                            </label>
+                            <Calendarpicker :required="store.form.valorEntrada != ''" id="dataEntradaLancamento"
+                                name="dataEntrada" v-model="store.form.dataEntrada" />
+                        </div>
+                    </div>
                     <!-- Forma de Pagamento -->
-                    <div>
+                    <div class="md:col-span-6">
                         <label for="formaPagamentoLancamento" class="block text-sm font-medium mb-1">
                             Forma de Pagamento *
                         </label>
@@ -194,32 +217,8 @@ async function submit() {
                             </SelectContent>
                         </Select>
                     </div>
-
-                    <div class="block text-sm font-medium mb-1">
-                        <span class="block text-sm font-medium mb-0">
-                            Efetivado
-                        </span>
-                        <div class="gap-2 mt-1 items-center">
-                            <div>
-                                <label for="lancamentoEfetivadoTotal"
-                                    class="border cursor-pointer bg-card dark:bg-card-dark border-border px-3 h-[36px] flex rounded-lg">
-                                    <div class="flex items-center">
-                                        <label class="relative inline-flex items-center cursor-pointer">
-                                            <Switch id="lancamentoEfetivadoTotal"
-                                                v-model="params.lancamentoEfetivado" />
-                                            <span
-                                                class="ml-2 text-sm font-medium text-gray-900 dark:text-gray-300">Efetivado</span>
-                                        </label>
-                                    </div>
-                                </label>
-                            </div>
-                        </div>
-                    </div>
-                </div>
-
-                <div class="grid grid-cols-1 md:grid-cols-2 gap-2">
                     <!-- Cliente -->
-                    <div>
+                    <div class="md:col-span-6">
                         <label for="clienteIdLancamento" class="block text-sm font-medium mb-1">
                             {{ store.form.tipo === 'RECEITA' ? 'Cliente' : 'Fornecedor' }}
                             <a @click="storeCliente.openSave" class="text-blue-500 px-2 cursor-pointer">+ Novo</a>
@@ -227,9 +226,8 @@ async function submit() {
                         <Select2Ajax id="clienteIdLancamento" v-model="store.form.clienteId" url="/clientes/select2"
                             allowClear />
                     </div>
-
                     <!-- Categoria -->
-                    <div>
+                    <div class="md:col-span-6">
                         <label for="categoriaIdLancamento" class="block text-sm font-medium mb-1">
                             Categoria *
                             <a onclick="openModalCategoriaFinanceira()" class="text-blue-500 px-2 cursor-pointer">+
@@ -238,20 +236,8 @@ async function submit() {
                         <Select2Ajax id="categoriaIdLancamento" v-model="store.form.categoriaId" required
                             url="lancamentos/categorias/select2" />
                     </div>
-                </div>
-
-                <div class="grid grid-cols-1 md:grid-cols-2 gap-2">
-                    <!-- Data Lançamento -->
-                    <div>
-                        <label for="dataFinanceiroLancamento" class="block text-sm font-medium mb-1">
-                            Data Lançamento *
-                        </label>
-                        <Calendarpicker id="dataFinanceiroLancamento" name="dataLancamento"
-                            v-model="store.form.dataLancamento" />
-                    </div>
-
                     <!-- Conta Financeiro -->
-                    <div>
+                    <div class="md:col-span-6">
                         <label for="contasFinanceiroId" class="block text-sm font-medium mb-1">
                             Conta Financeira *
                             <a onclick="openModalContasFinanceiras()" class="text-blue-500 px-2 cursor-pointer">+
@@ -261,7 +247,8 @@ async function submit() {
                             url="lancamentos/contas/select2" />
                     </div>
                 </div>
-                <div class="flex justify-end gap-2">
+
+                <div class="flex justify-end gap-2 mt-4">
                     <Button type="button" variant="secondary" @click="store.openModal = false">
                         Fechar
                     </Button>
