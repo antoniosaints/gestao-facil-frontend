b80400576361a6ee2ca221313366a99d9562112d
Antonio Costa dos Santos <costaantonio883@gmail.com>
2025-10-16 11:06:24 -0300

    feat(ui) ajustes de UI

--- src/pages/financeiro/lancamentos/AcompanhamentoMensal.vue
+++ src/pages/financeiro/lancamentos/AcompanhamentoMensal.vue
@@ -55,11 +55,11 @@ onMounted(carregarLancamentos)
 
 <template>
     <div class="space-y-4">
-        <div class="flex justify-between items-center mb-2 bg-background border px-4 rounded-md py-4">
+        <div class="flex justify-between items-center mb-2 bg-background border px-4 rounded-xl py-4">
             <button @click="navigateMonth('prev')">
                 <ArrowBigLeft class="w-5 h-5" />
             </button>
-            <h2 class="font-bold">{{ formatToCapitalize(format(currentMonth, "MMMM yyyy", { locale: ptBR })) }}</h2>
+            <h2 class="text-xl">{{ formatToCapitalize(format(currentMonth, "MMMM yyyy", { locale: ptBR })) }}</h2>
             <button @click="navigateMonth('next')">
                 <ArrowBigRight class="w-5 h-5" />
             </button>
@@ -74,19 +74,20 @@ onMounted(carregarLancamentos)
         <div v-else class="space-y-4">
             <Card v-for="dia in lancamentos" :key="dia.dia" class="bg-transparent border-none shadow-none">
                 <CardContent class="p-0 border-none">
-                    <p class="text-sm px-3">{{ format(new Date(dia.dia), "dd/MM/yyyy") }}</p>
+                    <p class="text-sm px-3 bg-card border py-1 mb-1 rounded-md">{{ format(new Date(dia.dia),
+                        "dd/MM/yyyy") }}</p>
                     <div class="flex flex-col gap-2">
                         <div v-for="item in dia.lancamentos" :key="item.id"
                             class="flex flex-col sm:flex-row justify-between py-1 pl-6 gap-2 bg-background border px-3 rounded-md relative">
                             <div v-if="item.tipo === 'RECEITA'"
-                                class="absolute left-0 top-0 w-4 h-full rounded-l-md bg-success/90"></div>
-                            <div v-else class="absolute left-0 top-0 w-4 h-full rounded-l-md bg-danger/90"></div>
+                                class="absolute left-0 top-0 w-2 h-full rounded-l-md bg-success/90"></div>
+                            <div v-else class="absolute left-0 top-0 w-2 h-full rounded-l-md bg-danger/90"></div>
                             <div>
                                 <div class="font-medium text-md">{{ item.descricao }}</div>
-                                <div class="text-xs text-gray-500">
+                                <div class="text-xs text-muted-foreground">
                                     {{ item.categoria }}
                                 </div>
-                                <div class="text-xs text-gray-500">
+                                <div class="text-xs text-muted-foreground">
                                     {{ item.tipo === 'RECEITA' ? 'Receita' : 'Despesa' }} · {{ item.status }}
                                 </div>
                             </div>
