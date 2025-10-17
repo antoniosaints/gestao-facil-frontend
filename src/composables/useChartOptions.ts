bd51c13deb10bc9a591aed4c2fa5ab151efd3261
Antonio Costa dos Santos <costaantonio883@gmail.com>
2025-10-17 15:24:10 -0300

    fix(ui) ajuste de cor dos graficos

--- src/composables/useChartOptions.ts
+++ src/composables/useChartOptions.ts
@@ -2,15 +2,17 @@ import { colorTheme } from '@/utils/theme'
 import { computed } from 'vue'
 
 const currentColor = computed(() => (colorTheme.value === 'dark' ? '#ffffff' : '#000000'))
+const currentColorMuted = computed(() => (colorTheme.value === 'dark' ? '#525252' : '#d1d1d1'))
 
 export const optionsChartBar = computed(() => ({
   responsive: true,
   interaction: { mode: 'index', intersect: false },
   plugins: { legend: { display: false } },
   scales: {
-    x: { ticks: { color: currentColor.value } },
+    x: { ticks: { color: currentColor.value }, grid: { color: currentColorMuted.value } },
     y1: {
       ticks: { color: currentColor.value },
+      grid: { drawOnChartArea: false, color: currentColorMuted.value },
       type: 'linear',
       position: 'left',
       title: { display: true, text: 'Valor Total (R$)', color: currentColor.value },
@@ -21,7 +23,7 @@ export const optionsChartBar = computed(() => ({
       type: 'linear',
       position: 'right',
       title: { display: true, text: 'Quantidade de Vendas', color: currentColor.value },
-      grid: { drawOnChartArea: false },
+      grid: { drawOnChartArea: false, color: currentColorMuted.value },
       stacked: false,
     },
   },
@@ -33,8 +35,8 @@ export const optionsChartBarDefault = computed(() => ({
   interaction: { mode: 'index', intersect: false },
   plugins: { legend: { display: false } },
   scales: {
-    y: { ticks: { color: currentColor.value } },
-    x: { ticks: { color: currentColor.value } },
+    y: { ticks: { color: currentColor.value }, grid: { color: currentColorMuted.value } },
+    x: { ticks: { color: currentColor.value }, grid: { color: currentColorMuted.value } },
   },
   elements: { bar: { borderRadius: 6 } },
 }))
@@ -50,12 +52,12 @@ export const optionsChartBarStack = computed(() => ({
     y: {
       stacked: true,
       ticks: { color: currentColor.value },
-      grid: { display: true },
+      grid: { display: true, color: currentColorMuted.value },
     },
     x: {
       stacked: true,
       ticks: { color: currentColor.value },
-      grid: { display: false },
+      grid: { display: false, color: currentColorMuted.value },
     },
   },
   elements: {
