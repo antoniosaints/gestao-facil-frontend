3c576f3cc1a7ae70344cdb26dc97b07ca96a4a7b
Antonio Costa dos Santos <costaantonio883@gmail.com>
2025-10-23 09:22:10 -0300

    feat(layout) adicionado fade de transição

--- src/App.vue
+++ src/App.vue
@@ -2,7 +2,9 @@
 import { useRoute } from 'vue-router'
 import Main from './layouts/Main.vue'
 import Default from './layouts/Default.vue'
+import { useUiStore } from './stores/ui/uiStore'
 
+const store = useUiStore()
 const route = useRoute()
 const layouts = {
   default: Default,
@@ -12,8 +14,35 @@ const layouts = {
 
 <template>
   <component :is="route.meta.layout ? layouts.main : layouts.default">
-    <RouterView />
+    <transition :name="store.isMobile ? 'fase' : 'slide'" mode="out-in">
+      <RouterView />
+    </transition>
   </component>
 </template>
 
-<style scoped></style>
+<style scoped>
+.fade-enter-active,
+.fade-leave-active {
+  transition: opacity 0.3s ease;
+}
+
+.fade-enter-from,
+.fade-leave-to {
+  opacity: 0;
+}
+
+.slide-enter-active,
+.slide-leave-active {
+  transition: all 0.4s ease;
+}
+
+.slide-enter-from {
+  opacity: 0;
+  transform: translateX(20px);
+}
+
+.slide-leave-to {
+  opacity: 0;
+  transform: translateX(-20px);
+}
+</style>
