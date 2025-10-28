f666b00e53770b7ea30bfde54ede8808b01dd8cd
Antonio Costa dos Santos <costaantonio883@gmail.com>
2025-10-28 17:58:11 -0300

    feat(ui) ajuste de UI

--- src/pages/financeiro/lancamentos/formulario/LancamentoModal.vue
+++ src/pages/financeiro/lancamentos/formulario/LancamentoModal.vue
@@ -316,7 +316,7 @@ watch(() => params.value.hasEntrada, () => {
                     </div>
                 </div>
 
-                <div class="flex flex-col bg-gray-50 border rounded-md px-4 py-2">
+                <div class="flex flex-col bg-gray-50 dark:bg-gray-900 border rounded-md px-4 py-2">
                     <span class="text-muted-foreground">Resumo do lançamento</span>
                     <span class="text-sm">Valor total:
                         {{ formatCurrencyBR(store.form.valorTotal) }}
