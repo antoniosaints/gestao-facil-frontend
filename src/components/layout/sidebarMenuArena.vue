9b9938dd1fbf4a2f863620f4dc9417abc821f152
Antonio Costa dos Santos <costaantonio883@gmail.com>
2025-11-26 12:41:56 -0300

    feat(ui) ajuste de UI

--- src/components/layout/sidebarMenuArena.vue
+++ src/components/layout/sidebarMenuArena.vue
@@ -14,11 +14,11 @@
             <div v-else-if="item.children && item.children.length" v-show="item.show !== false"
                 class="mb-1 cursor-pointer mt-2 rounded-xl dark:border-gray-500">
                 <button @click="toggleDropdown(item.nome)"
-                    class="dropdown-toggle w-full flex items-center bg-blue-900/30 justify-between px-4 py-2 text-left cursor-pointer rounded-xl"
+                    class="dropdown-toggle w-full flex items-center bg-yellow-900/30 justify-between px-4 py-2 text-left cursor-pointer rounded-xl"
                     :class="{ 'rounded-b-none': openDropdowns.includes(item.nome) }">
                     <div class="flex items-center space-x-3">
                         <i v-if="typeof item.icone === 'string'"
-                            :class="[item.icone, 'text-blue-800 dark:text-blue-400']"></i>
+                            :class="[item.icone, 'text-yellow-800 dark:text-yellow-400']"></i>
                         <component v-else :is="item.icone" :class="['w-5 h-5', 'text-gray-200 dark:text-gray-200']">
                         </component>
                         <span class="text-gray-200 dark:text-gray-300">{{ item.nome }}</span>
@@ -31,14 +31,14 @@
                 </button>
 
                 <div v-show="openDropdowns.includes(item.nome)"
-                    class="dropdown-content flex flex-col p-1 gap-1 bg-blue-900/75 rounded-b-xl">
+                    class="dropdown-content flex flex-col p-1 gap-1 bg-yellow-900/75 rounded-b-xl">
                     <router-link v-for="(child, i) in item.children" :key="i" :to="child.link || 'javascript:void(0)'"
                         v-show="child.show !== false" :class="[
-                            'flex items-center p-2 text-gray-200 dark:text-gray-300 pl-4 hover:bg-sidebar/80 cursor-pointer rounded-xl',
-                            route.path === child.link && 'bg-blue-700 dark:bg-blue-950'
+                            'flex items-center p-2 text-gray-200 dark:text-gray-300 pl-4 hover:bg-yellow-700/80 cursor-pointer rounded-xl',
+                            route.path === child.link && 'bg-yellow-700 dark:bg-yellow-950'
                         ]">
                         <i v-if="typeof child.icone === 'string'"
-                            :class="[child.icone, 'mr-2', 'text-blue-600 dark:text-blue-600']"></i>
+                            :class="[child.icone, 'mr-2', 'text-yellow-600 dark:text-yellow-600']"></i>
                         <component v-else :is="child.icone"
                             :class="['mr-2 w-5 h-5', 'text-gray-200 dark:text-gray-300']">
                         </component>
@@ -51,8 +51,8 @@
 
             <!-- Link normal -->
             <router-link
-                class="cursor-pointer dark:border-gray-500 mt-2 hover:bg-blue-900/50 text-gray-200 dark:text-gray-300 p-2 rounded-xl flex items-center"
-                :class="[route.path === item.link ? 'bg-blue-900/80' : 'bg-blue-900/30']" v-else
+                class="cursor-pointer dark:border-gray-500 mt-2 hover:bg-yellow-900/50 text-gray-200 dark:text-gray-300 p-2 rounded-xl flex items-center"
+                :class="[route.path === item.link ? 'bg-yellow-900/80' : 'bg-yellow-900/30']" v-else
                 v-show="item.show !== false" :to="item.link || 'javascript:void(0)'">
                 <i v-if="typeof item.icone === 'string'"
                     :class="[item.icone, 'px-2', 'text-gray-200 dark:text-gray-100']"></i>
@@ -75,8 +75,8 @@ const route = useRoute()
 const colorClasses = {
     red: "text-red-500",
     green: "text-green-500",
-    blue: "text-blue-500",
     yellow: "text-yellow-500",
+    blue: "text-blue-500",
     gray: "text-gray-500",
     orange: "text-orange-500",
     indigo: "text-indigo-500",
