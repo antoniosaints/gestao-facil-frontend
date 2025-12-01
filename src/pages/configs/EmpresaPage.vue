bcfb7eb77dc8ad0514b797b7a69ff0f8a5f5ef0f
Antonio Costa dos Santos <costaantonio883@gmail.com>
2025-12-01 18:05:57 -0300

    feat(ui) ajuste de UI

--- src/pages/configs/EmpresaPage.vue
+++ src/pages/configs/EmpresaPage.vue
@@ -6,7 +6,7 @@
         </CardHeader>
 
         <form @submit.prevent="submitForm">
-            <CardContent class="grid md:grid-cols-2 gap-6">
+            <CardContent class="grid md:grid-cols-2 gap-4">
                 <div class="space-y-2">
                     <Label for="razao">Razão Social *</Label>
                     <Input id="razao" placeholder="Empresa LTDA" required v-model="form.nome" />
@@ -43,6 +43,25 @@
                         </SelectContent>
                     </Select>
                 </div>
+                <!-- <div class="space-y-2">
+                    <Label for="linkColor">Cor do link</Label>
+                    <Select required>
+                        <SelectTrigger class="w-full">
+                            <SelectValue placeholder="Selecione uma cor" />
+                        </SelectTrigger>
+                        <SelectContent>
+                            <SelectGroup>
+                                <SelectItem v-for="cor in DEFAULT_COLORS" :key="cor.hex" :value="cor.hex">
+                                    <div class="flex items-center gap-2">
+                                        <div class="w-5 h-5 rounded border" :style="{ backgroundColor: cor.hex }" />
+                                        <div class="flex-1 text-sm">{{ cor.name }}</div>
+                                        <div class="text-xs text-muted-foreground">{{ cor.hex }}</div>
+                                    </div>
+                                </SelectItem>
+                            </SelectGroup>
+                        </SelectContent>
+                    </Select>
+                </div> -->
                 <div class="space-y-2">
                     <Label for="endereco">Endereço</Label>
                     <Input id="endereco" placeholder="Endereço completo" v-model="form.endereco" />
@@ -96,6 +115,31 @@ const form = ref<Partial<UpdateConta & { email: string, cep: string }>>({
     cep: "",
     email: "",
 })
+
+type ColorItem = { name: string; hex: string };
+
+const DEFAULT_COLORS: ColorItem[] = [
+    { name: "Black", hex: "#000000" },
+    { name: "White", hex: "#FFFFFF" },
+    { name: "Gray", hex: "#6B7280" },
+    { name: "Red", hex: "#EF4444" },
+    { name: "Orange", hex: "#F97316" },
+    { name: "Amber", hex: "#F59E0B" },
+    { name: "Yellow", hex: "#EAB308" },
+    { name: "Lime", hex: "#84CC16" },
+    { name: "Green", hex: "#22C55E" },
+    { name: "Emerald", hex: "#10B981" },
+    { name: "Teal", hex: "#14B8A6" },
+    { name: "Cyan", hex: "#06B6D4" },
+    { name: "Sky", hex: "#0EA5E9" },
+    { name: "Blue", hex: "#3B82F6" },
+    { name: "Indigo", hex: "#6366F1" },
+    { name: "Violet", hex: "#8B5CF6" },
+    { name: "Purple", hex: "#A78BFA" },
+    { name: "Pink", hex: "#EC4899" },
+    { name: "Rose", hex: "#F43F5E" },
+];
+
 
 const submitForm = async () => {
     try {
